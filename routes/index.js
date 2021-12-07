134e4e5ee967ba7536862d3f26e0b00dcced0069
cypher20b <nuel.emma20@gmail.com>
2021-12-06 18:26:12 -0800

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var verified;
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/verify',(req, res)=>{
+  verified = req.body
+})
+
+router.get('/paystack', (req, res)=>{
+  res.send(verified)
+})
+
+module.exports = router;
