ddfe5ea8e1e52d8a149a818c86b19ab182f0625b
cypher20b <nuel.emma20@gmail.com>
2021-12-06 19:41:35 -0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var verified = '';
+var verified;
+let pdfname
+let resdata
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -12,6 +14,36 @@ router.post('/verify',(req, res)=>{
 
 router.get('/paystack', (req, res)=>{
   res.send(verified)
+})
+
+router.post('/multipleFiles', upload.array('files'), (req, res, next) => {
+  const files = req.files;
+  console.log(files[0].filename);
+  pdfname = files[0].filename
+  const  childpython = spawn('python', ['script1.py', `./uploads/${pdfname}`]);
+  childpython.stdout.on('data', (data)=>{
+    console.log(data.indexOf("Education"));//find first occurance of a word 
+    console.log(`${data}`);
+    resdata=`${data}`
+    res.json(resdata)
+    // res.send('passed sucessfully')
+})
+
+childpython.stderr.on('data', (data)=>{
+    console.log(`${data}`);
+    // res.send(data)
+})
+
+childpython.on('close', (code)=>{
+    console.log(`python script exited with code ${code}`);
+})
+  if (!files) {
+    const error = new Error('No File')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    // res.send({sttus:  'ok'});
+    // res.send(resdata)
 })
 
 module.exports = router;
---
+++ script1.py
@@ -0,0 +1,15 @@
+import sys
+import json
+from PyPDF2 import PdfFileReader
+from pathlib import Path
+pdf = PdfFileReader(str(sys.argv[1]))
+
+page_1_text= pdf.getPage(0).extractText()
+# print(page_1_object)
+text=''
+for page in pdf.pages:
+        text += page.extractText()
+#print(json.dumps(text)) #<json version of the parsed pdf>
+print(text) 
+sys.stdout.flush()
+print('python script running cool ', str(sys.argv[1]))
