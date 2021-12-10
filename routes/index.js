61da9bb2baef83b51663e957d763fea603ae59f2
cypher20b <nuel.emma20@gmail.com>
2021-12-10 10:17:41 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -92,7 +92,7 @@ router.post("/multipleFiles", upload.array("files"), (req, res, next) => {
 
 
 
-router.post("/idleNewmultipleFiles", upload.array("files"), (req, res, next) => {
+router.post('/newmultipleFiles', upload.array("files"), (req, res, next) => {
   const files = req.files;
   console.log(files[0].filename);
   console.log(user.Name);
@@ -108,7 +108,7 @@ router.post("/idleNewmultipleFiles", upload.array("files"), (req, res, next) => 
    res.json('upload failed')
  }
 });
-router.get('/newmultipleFiles', (req, res, next) => {
+router.get('/NExtwmultipleFiles', (req, res, next) => {
   res.json('respindein')
 });
 
