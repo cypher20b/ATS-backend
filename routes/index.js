a1ab69e820c339fa050e4ee4e96fedafd3914add
cypher20b <nuel.emma20@gmail.com>
2021-12-10 09:51:21 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,29 @@ var express = require('express');
 var router = express.Router();
 const multer = require('multer');
 const {spawn} = require('child_process');
-var verified='before';
-let pdfname
+const { urlencoded } = require('express');
+var verified='';
+let pdfname=''
 let resdata
-
+let user = {
+  "Name":'',
+  "PdfName":'',
+  "status":0,
+  "reference":''
+}
+let referenceCode
+let processedData = {
+  "length":0,
+  "education":'',
+  "personalInfo":'',
+  "experience":'',
+  "Roles":'',
+  "Contact":'',
+  "Email":'',
+  "Languages":'',
+  "Date of Birth":'',
+  "Project":'',
+}
 const storage = multer.diskStorage({
   destination: (req, file, callBack) => {
       callBack(null, 'uploads')
@@ -23,45 +42,75 @@ router.get('/', function(req, res, next) {
 
 router.post('/verify',(req, res)=>{
   verified = req.body
-  console.log(req);
 })
 
-router.get('/paystack', (req, res)=>{
-  res.json(verified)
+router.get('/paystack',(req, res)=>{
+    res.send(verified)
+    res.end()
+ 
+  // res.end()
 })
 
-router.post('/multipleFiles', upload.array('files'), (req, res, next) => {
+router.post("/multipleFiles", upload.array("files"), (req, res, next) => {
   const files = req.files;
   console.log(files[0].filename);
-  pdfname = files[0].filename
-  const  childpython = spawn('python', ['script1.py', `./uploads/${pdfname}`]);
-  childpython.stdout.on('data', (data)=>{ 
-   console.log(data.indexOf("Education")); //find first occurance of a word
+  pdfname = files[0].filename;
+  const childpython = spawn("python", ["script1.py", `./uploads/${pdfname}`]);
+  childpython.stdout.on("data", (data) => {
+    console.log(data.indexOf("Education")); //find first occurance of a word
     processedData.length = data.length;
-    processedData.education=data.match(/ain/gi);
-    processedData.experience=data.includes("world");
+    processedData.experience=data.includes("experience");
+    processedData.education=data.includes("education");
+    processedData.Contact=data.includes("Contact");
+    processedData.Roles=data.includes("Roles");
+    processedData.Email=data.includes("Email");
+    processedData.education=data.includes("education");
     // console.log(`${data}`);
     // console.log(res);
     resdata = `${data}`;
-    res.json(processedData);
+    // res.json(resdata);
+    res.json(processedData)
     // res.json('passed sucessfully')
   });
 
-childpython.stderr.on('data', (data)=>{
+  childpython.stderr.on("data", (data) => {
     console.log(`${data}`);
     // res.send(data)
-})
+  });
 
-childpython.on('close', (code)=>{
+  childpython.on("close", (code) => {
     console.log(`python script exited with code ${code}`);
-})
+  });
   if (!files) {
-    const error = new Error('No File')
-    error.httpStatusCode = 400
-    return next(error)
+    const error = new Error("No File");
+    error.httpStatusCode = 400;
+    return next(error);
   }
-    // res.send({sttus:  'ok'});
-    // res.send(resdata)
-})
+  // res.send({sttus:  'ok'});
+  // res.send(resdata)
+});
 
+
+
+router.post("/newmultipleFiles", upload.array("files"), (req, res, next) => {
+  const files = req.files;
+  console.log(files[0].filename);
+  console.log(user.Name);
+  pdfname = files[0].filename;
+ if (pdfname) {
+  referenceCode = Math.random().toString(36).substr(2)
+  user.Name = req.body.name
+   user.PdfName = pdfname
+   user.status = 1
+   user.reference = referenceCode
+   res.json(user)
+ } else {
+   res.json('upload failed')
+ }
+});
+
+
+router.get("/confirmPay", (req, res, next) => {
+  res.json('respindein')
+});
 module.exports = router;
