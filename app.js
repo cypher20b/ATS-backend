fa00aa4a870ec24bdededab8772a7f5889e8901d
cypher20b <nuel.emma20@gmail.com>
2024-03-29 08:41:07 +0000

    fix: changed route names to suite the new changes

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require('./routes/user');
+const usersRouter = require('./routes/applicant');
 const bodyParser = require('body-parser');
 var app = express();
 app.use(cors())
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/applicant', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
