245691379347df5370436f03c2ad9608809397f6
cypher20b <nuel.emma20@gmail.com>
2024-03-29 08:44:15 +0000

    feat: added a route to make job hunters able to apply to a job with their cv

---
+++ routes/applicant.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+const multer = require('multer');
+const {spawn} = require('child_process');
+var nodemailer = require('nodemailer');
+var mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const storage = multer.diskStorage({
+  destination: (req, file, callBack) => {
+      callBack(null, 'uploads')
+  },
+  filename: (req, file, callBack) => {
+      callBack(null, `CV_${file.originalname}`)
+  }
+})
+const upload = multer({ storage: storage })
+const client =mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'eaweb_atsdb'
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express on Applicants route' });
+});
+
+router.post("/job-apply", upload.array("files"), (req, res, next) => {
+  console.log(req.files)
+    client.query(
+      `REPLACE INTO applicants (FirstName, LastName, Email,Phone, Resume, CoverLetter)
+      VALUES (?, ?, ?, ?, ?, ?)`,
+      [req.body.firstName, req.body.lastName, req.body.email, req.body.phone, req.files[0]?.originalname, req.files[1]?.originalname],
+      (err, result) => {
+        if (err) {
+          res.status(500).json({
+            code: 500,
+            message: 'Error occurred while inserting data into the database',
+            error: err.message
+          });
+        } else {
+          client.query(
+            `SELECT * FROM applicants WHERE ApplicantID = ${result.insertId}`,
+            (err, results) => {
+              if (err) {
+                res.status(500).json({
+                  code: 500,
+                  message: 'Error occurred while fetching inserted data',
+                  error: err.message
+                });
+              } else {
+                res.status(200).json({
+                  code: 200,
+                  message: 'Data inserted successfully',
+                  data: results[0] 
+                });
+              }
+            }
+          );
+        }
+      }
+    );
+});
+module.exports = router;
