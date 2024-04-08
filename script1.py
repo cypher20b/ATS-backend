57057494bb88ecfa89f5e64b5d402054ed37b449
cypher20b <nuel.emma20@gmail.com>
2024-04-08 11:25:38 +0100

    feat: modified cv parsing algorithm to return a numerical value

--- script1.py
+++ script1.py
@@ -1,32 +1,38 @@
-import sys
-import json
-from PyPDF2 import PdfFileReader
-from pathlib import Path
+import PyPDF2
 import nltk
+import sys
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from collections import Counter
-pdf = PdfFileReader(str(sys.argv[1]))
 
-page_1_text= pdf.getPage(0).extractText()
-# print(page_1_object)
-text=''
-for page in pdf.pages:
-        text += page.extractText()
-#print(json.dumps(text)) #<json version of the parsed pdf>
-def extract_keywords(txt):
-        tokens - word_tokenize(txt)
-        stop_words = set(stopwords.words('english'))
-        filtered_tokens = [word.lower() for word in tokens if word.lower() not in stop_words and word.isalnum()]
-        lemmatizer - WordNetLemmatizer()
-        lemmatized_tokens = [lemmatizer.lemmatize(word) for word in filtered_tokens]
-        num_keywords = 10
-        keywords = word_freq.most_common(num_keywords)
-        return keywords
+def extract_text_from_pdf(pdf_file):
+    text = ""
+    with open(pdf_file, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            text += page.extract_text()
+    return text
 
-keywd = extract_keywords(text)
-print(keywd) 
-print(text) 
-sys.stdout.flush()
-# print('python script running cool ', str(sys.argv[1]))
+def extract_keywords(text):
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word.lower() for word in tokens if word.lower() not in stop_words and word.isalnum()]
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(word) for word in filtered_tokens]
+    word_freq = Counter(lemmatized_tokens)
+    return word_freq
+
+def score_pdf(job_description, pdf_text):
+    job_keywords = extract_keywords(job_description)
+    pdf_keywords = extract_keywords(pdf_text)
+    common_keywords = job_keywords.keys() & pdf_keywords.keys()
+    score = sum(job_keywords[key] for key in common_keywords)
+    return score
+
+job_description = sys.argv[2]
+pdf_file = sys.argv[1]
+pdf_text = extract_text_from_pdf(pdf_file)
+pdf_score = score_pdf(job_description, pdf_text)
+print(pdf_score)
